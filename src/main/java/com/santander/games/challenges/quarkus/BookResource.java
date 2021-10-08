0a7eaaaf1cd297239f150b5f9ab01a430fa10128
Auri Munoz <amunozhe@redhat.com>
2021-10-08 17:18:33 +0200

    feat: implement delete by id method. Fixes #7

--- src/main/java/com/santander/games/challenges/quarkus/BookResource.java
+++ src/main/java/com/santander/games/challenges/quarkus/BookResource.java
@@ -1,10 +1,7 @@
 package com.santander.games.challenges.quarkus;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -31,6 +28,12 @@ public class BookResource {
         return Book.findByPublicationYearBetween(lower,higher);
     }
 
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteById(@PathParam("id")Integer id){
+        Book.deleteById(1);
+    }
 
 
     @GET
